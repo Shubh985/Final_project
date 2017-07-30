b2fee9b8b6946293e08b7cc012e923e814ccfa24
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-30 23:56:41 +0530

    secobnd migrations

--- p2p/market/models.py
+++ p2p/market/models.py
@@ -11,3 +11,23 @@ class UserModel (models.Model):
     password = models.CharField(max_length=40)
     created_on = models.DateTimeField(auto_now_add = True)
     updated_on = models.DateTimeField(auto_now = True)
+
+    # A session model
+    # it will store the session details with user field
+    # a foreign key which is the above defined model
+    # it has a method create_token which assigns an unique value to session token
+
+
+class SessionToken(models.Model):
+    user = models.ForeignKey(UserModel)
+    session_token = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    is_valid = models.BooleanField(default=True)
+
+    def create_token(self):
+        self.session_token = uuid.uuid4()
+
+
+
+
+
