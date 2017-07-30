94b3cbcd42e5afca10777442a6bbfb54fac66ad1
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-30 23:40:59 +0530

    login fail id done

--- p2p/p2p/urls.py
+++ p2p/p2p/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from views import signup_view,login_view
+from views import signup_view,login_view,login_fail,login_success
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',signup_view),
-    url(r'^login/',login_view)
+    url(r'^sign',signup_view),
+    url(r'^login/',login_view),
+    url(r'^fail/',login_fail),
+    url(r'^success/',login_success),
     ]
--- p2p/p2p/views.py
+++ p2p/p2p/views.py
@@ -10,3 +10,9 @@ def signup_view(request):
 
 def login_view(request):
     return render(request,'login.html')
+
+def login_fail(request):
+    return render(request,'loginfail.html')
+
+def login_success(request):
+    return render(request,'login_success.html')
