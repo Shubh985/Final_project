f706a607787c2531696c3a372986ecc6a9a90c1b
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-31 08:54:31 +0530

    first.tml

--- p2p/p2p/urls.py
+++ p2p/p2p/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from views import signup_view,login_view,login_fail,login_success
+from views import signup_view
+from views import login_view,feed_view,post_view,like_view,comment_view,welcome_view,logout_view
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',signup_view),
-    url(r'^login/',login_view),
-    url(r'^fail/',login_fail),
-    url(r'^success/',login_success),
-    ]
+    url(r'^signup/', signup_view),
+    url(r'^login/', login_view),
+    url(r'^feed/',feed_view),
+    url(r'^post/',post_view),
+    url(r'^like/', like_view),
+    url(r'^logout/',logout_view),
+    url(r'^comment/',comment_view),
+    url(r'',welcome_view),
+
+]
